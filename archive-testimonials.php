15f997952af0e4de58f0512031a2ac74f86c831a
Lisa Wells <lisawells999@gmail.com>
2016-09-30 13:25:18 -0400

    add bkgd image archive testimonials

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -5,10 +5,6 @@
 //* Remove the entry title (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
-//* Remove the entry header markup (requires HTML5 theme support)
-// remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
-// remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
-
 //* Remove the entry meta in the entry header (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
