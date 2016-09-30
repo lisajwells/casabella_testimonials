895e544e323a1f7795cd309722a69734699915ff
Lisa Wells <lisawells999@gmail.com>
2016-09-30 14:44:18 -0400

    style overline and title archive testimonials

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -8,11 +8,10 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 //* Remove the entry meta in the entry header (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
-add_action( 'genesis_after_header', 'casa_show_testimonial_page_title' );
+add_action( 'genesis_before_content_sidebar_wrap', 'casa_show_testimonial_page_title' );
 function casa_show_testimonial_page_title() {
-    echo '<p>What people are saying</p><h2 class="title">Featured Testimonials</h2>';
+    echo '<p class="overline"><span>What people are saying</span></p><h2 class="title">Featured Testimonials</h2>';
 }
-// echo <p>What people are saying</p><h2 class="title">Featured Testimonials</h2>
 
 // Initialize Genesis.
 genesis();
